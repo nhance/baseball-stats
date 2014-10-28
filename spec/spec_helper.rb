0f72a81abb2c54cd99061e21083dc81bb2afbf11
Nicholas Hance <nhance@reenhanced.com>
2014-10-28 15:25:02 -0400

    Removes database_helpers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,5 +6,4 @@ require 'baseball-stats'
 Dir[File.expand_path('../support/**/*.rb', __FILE__)].each {|f| require f}
 
 RSpec.configure do |config|
-  config.include DatabaseHelpers
 end
--- spec/support/database_helpers.rb
+++
@@ -1,8 +0,0 @@
-module DatabaseHelpers
-  def init_db!
-    File.delete(File.join(__dir__, '../../db/test.sqlite3')) # Hardcoded to save time
-    BaseballStats::Application.new('test')
-    migrations_dir = File.join(__dir__, '../../db/migrate/*.rb')
-    ActiveRecord::Migrator.migrate migrations_dir
-  end
-end
